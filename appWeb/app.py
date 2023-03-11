7e9070571cad5c598f606a707684528b8952dc37
j2sanabriam <jj.sanabria@uniandes.edu.co>
2023-03-10 20:47:01 -0500

    pag web v 1.1

--- appWeb/app.py
+++ appWeb/app.py
@@ -1,35 +1,129 @@
 from flask import Flask, render_template, request, send_from_directory
-from models import prediction
+import models as mdl
+from datetime import date
 import pandas as pd
 
 app = Flask(__name__, static_url_path='/static')
-userid = ""
 
-@app.route("/" , methods=['GET'])
+user_id = ""
+user_gender = ""
+user_age = 0
+user_country = ""
+user_date = ""
+
+users_db = mdl.getUsers()
+# items_db = mdl.getItems()
+
+
+@app.route("/", methods=['GET'])
 def login():
-    global userid
-    userid = ""
+    global users_db, user_id, user_gender, user_age, user_country, user_date
+    user_id = ""
+    user_gender = ""
+    user_age = 0
+    user_country = ""
+    user_date = ""
+
+    print(type(request))
+    print(request)
+    print(request.args)
+
+    print("Ingresa Get")
     return render_template("login.html")
 
-@app.route("/register" , methods=['GET'])
-def register():
-    return render_template("register.html")
+@app.route("/create", methods=['GET'])
+def create():
+    global users_db, user_id, user_gender, user_age, user_country, user_date, url
+    # limpiar variables para login con usuario nuevo
+    user_id = ""
+    user_gender = ""
+    user_age = 0
+    user_country = ""
+    user_date = ""
 
-@app.route("/index" , methods=['GET'])
-def index():
-    global userid
-    if userid == "":
-        userid = request.args.get('loginUser')
-    if userid == "Juan":
-        imagen = 'src=./static/img/undraw_profile_m.svg'
+    # Leer valores desde los campos del formulario de creación
+    crea_user_id = request.args.get('createUserId')
+    crea_gender = request.args.get('createGender')
+    crea_age = request.args.get('createAge')
+    crea_country = request.args.get('createCountry')
+
+    # Si algún valor está vacío, genera mensaje de error y no crea usuario
+    if not crea_user_id or not crea_gender or not crea_age or not crea_country:
+        return render_template("404.html", mensaje="Datos Incompletos")
     else:
-        imagen = 'src=./static/img/undraw_profile_f.svg'
-    return render_template("index.html", usuario=userid, imagen=imagen)
+        # Valida si el usuario a crear ya existe
+        user_row = users_db[users_db['#id'] == crea_user_id].reset_index()
+        if len(user_row) > 0:
+            return render_template("404.html", mensaje="Usuario Ya Existe")
+        else:
+            # Fecha de creación en string
+            today = date.today()
+            crea_date = today.strftime('%b') + " " + str(today.strftime('%d')) + ", " + str(today.strftime('%Y'))
 
+            # Inserta registro nuevo usuario a dataframe
+            new_row = {'#id': crea_user_id, 'gender': crea_gender, 'age': float(crea_age), 'country': crea_country,
+                       'registered': crea_date}
+            users_db = users_db.append(new_row, ignore_index=True)
+
+            # Guarda dataframe a archivo plano
+            users_db.to_csv(url, sep="\t", index=False)
+            return render_template("login.html")
+
+
+@app.route("/register", methods=['GET'])
+def register():
+    top_ten = mdl.getTopTen()
+    return render_template("register.html", top10=top_ten)
+
+
+@app.route("/index", methods=['GET'])
+def index():
+    global users_db, user_id, user_age, user_gender, user_country, user_date
+
+    print(type(request))
+    print(request)
+    print(request.args)
+
+    if user_id == "":
+        user_id = request.args.get('loginUser')
+
+    user_row = users_db[users_db['#id'] == user_id].reset_index()
+    if len(user_row) > 0:
+        user_gender = user_row["gender"].values[0]
+        user_age = user_row["age"].values[0]
+        user_country = user_row["country"].values[0]
+        user_date = user_row["registered"].values[0]
+
+        profile_img = mdl.getProfileImg(user_gender)
+
+        fav = mdl.getFavoriteArts()
+        rec = mdl.getRecomendations()
+        sim_users = mdl.getSimilarUsers()
+
+        return render_template("index.html", usuario=user_id, imagen=profile_img, favorites=fav, recommd=rec,
+                           similarUsers=sim_users)
+    else:
+        return render_template("404.html", mensaje="Usuario No Existe")
+
+
+@app.route("/profile", methods=['GET'])
+def profile():
+    global user_id, user_gender, user_age, user_country, user_date
+
+    profile_img = mdl.getProfileImg(user_gender)
+    genero_text = mdl.getGenderText(user_gender)
+
+    return render_template("profile.html", usuario=user_id, imagen=profile_img, genero=genero_text, edad=user_age,
+                           pais=user_country, fecha=user_date)
+
+
+@app.route("/error", methods=['GET'])
+def error():
+    return render_template("404.html", mensaje="404")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
 # if __name__ == "__main__":
 #     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
--- appWeb/models.py
+++ appWeb/models.py
@@ -2,12 +2,93 @@ import pandas as pd
 from prediction_model import PredictionModel
 from joblib import load
 
+
 def toJson(dataframe):
     result = dataframe.to_dict()
     return result
+
 
 def prediction(df):
     predicion_model = PredictionModel()
     results = predicion_model.make_predictions(df)
     return results.tolist()
 
+def getUsers():
+    url = "./static/data/userid-profile.tsv"
+    users_db = pd.read_csv(url, sep="\t")
+    return users_db
+
+def getItems():
+    url2 = "./static/data/userid-timestamp-artid-artname-traid-traname.tsv"
+    items_db = pd.read_csv(url2, sep="\t")
+    return items_db
+
+def getProfileImg(gender):
+    profile_img = 'src=./static/img/undraw_profile_m.svg'
+    if gender == "f":
+        profile_img = 'src=./static/img/undraw_profile_f.svg'
+    return profile_img
+
+def getGenderText(gender):
+    genero_text = ""
+    if gender == "f":
+        genero_text = "Femenino"
+    elif gender == "m":
+        genero_text = "Masculino"
+    return genero_text
+
+def getTopTen():
+    top_ten = []
+    top_ten.append(["Id Artista 1", "Nombre Artista 1", "Id Canción 1", "Nombre Canción 1"])
+    top_ten.append(["Id Artista 2", "Nombre Artista 2", "Id Canción 2", "Nombre Canción 2"])
+    top_ten.append(["Id Artista 3", "Nombre Artista 3", "Id Canción 3", "Nombre Canción 3"])
+    top_ten.append(["Id Artista 4", "Nombre Artista 4", "Id Canción 4", "Nombre Canción 4"])
+    top_ten.append(["Id Artista 5", "Nombre Artista 5", "Id Canción 5", "Nombre Canción 5"])
+    top_ten.append(["Id Artista 6", "Nombre Artista 6", "Id Canción 6", "Nombre Canción 6"])
+    top_ten.append(["Id Artista 7", "Nombre Artista 7", "Id Canción 7", "Nombre Canción 7"])
+    top_ten.append(["Id Artista 8", "Nombre Artista 8", "Id Canción 8", "Nombre Canción 8"])
+    top_ten.append(["Id Artista 9", "Nombre Artista 9", "Id Canción 9", "Nombre Canción 9"])
+    top_ten.append(["Id Artista 10", "Nombre Artista 10", "Id Canción 10", "Nombre Canción 10"])
+    return top_ten
+
+def getFavoriteArts():
+    fav_arts = []
+    fav_arts.append(["Id Artista 1", "Nombre Artista 1", "Conteo 1"])
+    fav_arts.append(["Id Artista 2", "Nombre Artista 2", "Conteo 2"])
+    fav_arts.append(["Id Artista 3", "Nombre Artista 3", "Conteo 3"])
+    fav_arts.append(["Id Artista 4", "Nombre Artista 4", "Conteo 4"])
+    fav_arts.append(["Id Artista 5", "Nombre Artista 5", "Conteo 5"])
+    fav_arts.append(["Id Artista 6", "Nombre Artista 6", "Conteo 6"])
+    fav_arts.append(["Id Artista 7", "Nombre Artista 7", "Conteo 7"])
+    fav_arts.append(["Id Artista 8", "Nombre Artista 8", "Conteo 8"])
+    fav_arts.append(["Id Artista 9", "Nombre Artista 9", "Conteo 9"])
+    fav_arts.append(["Id Artista 10", "Nombre Artista 10", "Conteo 10"])
+    return fav_arts
+
+def getRecomendations():
+    recommd = []
+    recommd.append(["Id Artista 1", "Nombre Artista 1", "Rating 1"])
+    recommd.append(["Id Artista 2", "Nombre Artista 2", "Rating 2"])
+    recommd.append(["Id Artista 3", "Nombre Artista 3", "Rating 3"])
+    recommd.append(["Id Artista 4", "Nombre Artista 4", "Rating 4"])
+    recommd.append(["Id Artista 5", "Nombre Artista 5", "Rating 5"])
+    recommd.append(["Id Artista 6", "Nombre Artista 6", "Rating 6"])
+    recommd.append(["Id Artista 7", "Nombre Artista 7", "Rating 7"])
+    recommd.append(["Id Artista 8", "Nombre Artista 8", "Rating 8"])
+    recommd.append(["Id Artista 9", "Nombre Artista 9", "Rating 9"])
+    recommd.append(["Id Artista 10", "Nombre Artista 10", "Rating 10"])
+    return recommd
+
+def getSimilarUsers():
+    similar = []
+    similar.append(["Usuario 1"])
+    similar.append(["Usuario 2"])
+    similar.append(["Usuario 3"])
+    similar.append(["Usuario 4"])
+    similar.append(["Usuario 5"])
+    similar.append(["Usuario 6"])
+    similar.append(["Usuario 7"])
+    similar.append(["Usuario 8"])
+    similar.append(["Usuario 9"])
+    similar.append(["Usuario 10"])
+    return similar
